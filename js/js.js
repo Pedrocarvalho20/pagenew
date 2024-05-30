25931a439be44eb1767e2403053e98fc06b1a906
Pedro Carvalho <pedrocarvalho3232@hotmail.com>
2024-05-30 16:23:50 -0300

    pagenew5

--- js/js.js
+++ js/js.js
@@ -19,8 +19,39 @@ function moveItemOnClick(type) {
     if (type === 'next') {
         thumb.appendChild(thumbItem[0]);
         list.appendChild(listItem[0]);
+        container.classlist.add('next')
     } else {
         thumb.prepend(thumbItem[thumbItem.length - 1]);
         list.prepend(listItem[listItem.length - 1]);
+        container.classlist.add('back')
     }
+
+    setTimeout(() => {
+        container.classlist.remove('next')
+        container.classlist.remove('back')
+    }, 3000);
 }
+ document.addEventListener("DOMContentLoaded", function() {
+        const listItems = document.querySelectorAll(".list-item");
+        
+        // Animação de entrada
+        listItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add("fadeIn");
+            }, index * 200); // Atraso entre cada item para animação em cascata
+        });
+
+        // Animação de saída ao clicar no botão "back"
+        document.querySelector('.back').addEventListener('click', () => {
+            listItems.forEach((item, index) => {
+                setTimeout(() => {
+                    item.classList.add("fade-out");
+                }, index * 200); // Atraso entre cada item para animação em cascata
+            });
+
+            // Redirecionar ou realizar alguma ação após a animação de saída
+            setTimeout(() => {
+                window.history.back(); // Voltar para a página anterior
+            }, listItems.length * 200 + 1000); // Tempo suficiente para a animação de saída concluir
+        });
+    });
